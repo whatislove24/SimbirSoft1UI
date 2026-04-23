31e89d29838acb88ec40722518c9af4dd143aade
whatislove24 <269238946+whatislove24@users.noreply.github.com>
2026-04-23 14:56:07 +0300

    обновление главной страницы

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,8 +1,13 @@
 package pages;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.TestConfig;
 
 import java.time.Duration;
 
@@ -23,11 +28,44 @@ public abstract class BasePage {
         driver.get(url);
     }
 
+    protected void openPath(String path) {
+        driver.get(TestConfig.BASE_URL + path);
+    }
+
     protected String getTitle() {
         return driver.getTitle();
     }
 
     protected String getCurrentUrl() {
         return driver.getCurrentUrl();
+    }
+
+    protected void type(WebElement element, String value) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        element.clear();
+        element.sendKeys(value);
+    }
+
+    protected void click(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    protected void selectByText(WebElement element, String text) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+        new Select(element).selectByVisibleText(text);
+    }
+
+    protected void scrollIntoView(WebElement element) {
+        ((JavascriptExecutor) driver)
+                .executeScript("arguments[0].scrollIntoView({block:'center'});", element);
+    }
+
+    protected void jsClick(WebElement element) {
+        ((JavascriptExecutor) driver)
+                .executeScript("arguments[0].click();", element);
+    }
+
+    protected void refreshPage() {
+        driver.navigate().refresh();
     }
 }
---
+++ src/test/java/utils/TestConfig.java
@@ -0,0 +1,9 @@
+package utils;
+
+public final class TestConfig {
+
+    private TestConfig() {
+    }
+
+    public static final String BASE_URL = "https://automationteststore.com/";
+}
